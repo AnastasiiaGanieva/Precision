e710ef0a6517790996dc1cdaa265c2d213d8c4df
AnastasiiaGanieva <69317903+AnastasiiaGanieva@users.noreply.github.com>
2021-03-06 01:02:13 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
-public class Main{
-public static void main(String[] args){
-System.out.println("Hello world!");
-}
+public class Main {
+    public static void main(String[] args) {
+        double regularBonus = 0.3;
+        double specialBonus = 0.6;
+        double totalBonus = regularBonus + specialBonus;
+        System.out.println(totalBonus);
+    }
 }
